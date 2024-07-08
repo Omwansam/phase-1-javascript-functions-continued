4849878c727f7be04484f53236a7eb69c13e49e1
Omwansam <omwansamarnold@gmail.com>
2024-07-08 10:46:57 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+      activity = "roller-skate";
+    }
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun("bathe my dog"));
+  console.log(saturdayFun());
+  
+  function mondayWork(activity) {
+    if (activity === undefined) {
+      activity = "go to the office";
+    }
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  console.log(saturdayFun("work from home"));
+  console.log(saturdayFun());
+  
+  function wrapAdjective(visual = "*") {
+    return function(adj = "special") {
+      return `You are ${visual}${adj}${visual}!`;
+    };
+  }
+  
+  const encouragingFunction = wrapAdjective("!!!")
+  console.log(encouragingFunction(" Dedicated programmer"));
